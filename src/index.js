17d3622bb776375c8a0e6294bd7ab8a50049037e
yakubalkis <yakupalkiis@gmail.com>
2022-08-29 22:19:37 +0300

    Made the App surrounded with Provider.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { legacy_createStore as createStore, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { reducer } from './redux/reducers'
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+                <Provider store={store} >
+                    <App/>
+                </Provider>   
+                    , document.getElementById('root'))
