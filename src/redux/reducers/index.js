83155897575a8087aee234de358307ac1c2664ea
yakubalkis <yakupalkiis@gmail.com>
2022-08-30 21:17:42 +0300

    Added showSideBar reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,14 @@
 const INITIAL_STATE = {
-    isToggle: false
+    isToggle: false,
+    isShow:false
 }
 
 export const reducer = ( state = INITIAL_STATE, action ) => {
     switch(action.type){
         case 'TOGGLE':
             return{...state, isToggle:!state.isToggle}
+        case 'SHOW_SIDEBAR':
+            return{...state, isShow:!state.isShow}
         default:
             return state
     }
