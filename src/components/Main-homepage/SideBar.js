20c4095c6bb7970f2aff9363a6bbdfbf423f15f6
yakubalkis <yakupalkiis@gmail.com>
2022-08-30 21:16:33 +0300

    Cancel and X buttons made dynamic.

--- src/components/Main-homepage/SideBar.js
+++ src/components/Main-homepage/SideBar.js
@@ -1,33 +1,50 @@
-import React,{ useRef} from "react";
+import React,{ useState} from "react";
 import xIconDark from '../img/x-iconDark.png'
 import xIconLight from '../img/x-iconLight.png'
 import cancelIconDark from '../img/cancelDark.png'
 import cancelIconLight from '../img/cancelLight.png'
 import saveIcon from '../img/icon-save.png'
+import { showSideBar } from "../../redux/actions";
 import { connect } from "react-redux";
 
 
+
 function SideBar(props){
-    const selectRef = useRef()
+    const [isDisableSelectCountry, setIsDisableSelectCountry ] = useState(true)
+    const [isDisableSelectCity, setIsDisableSelectCity] = useState(true)
     const theme = props.isToggle ? 'dark':'light'
     const xIcon = props.isToggle ? xIconDark : xIconLight
     const cancelIcon = props.isToggle ? cancelIconDark : cancelIconLight
-    
+
   
-    function handleSelect(e){
-        selectRef.current.disabled = e.target.value !== 'no select' ? false : true
+    
+    function handleSelectCountry(e){
+        if(e.target.value !== 'no select'){
+            setIsDisableSelectCountry(false)
+        }
+        else{
+            setIsDisableSelectCountry(true)
+            setIsDisableSelectCity(true)
+        }
     }
- 
+    function handleSelectCity(e){
+        if(e.target.value !== 'no select'){
+            setIsDisableSelectCity(false)
+        }
+        else{setIsDisableSelectCity(true)}
+    }
+  
+   
     return(
         <div className={`sidebar-homepage ${theme}-modeSidebar`}>
             <div className="sidebar-homepage-elements">
                 <div className="sidebar-header">
                     <h3>Add City</h3>
-                    <img alt="" className="btn" src={xIcon} />
+                    <img alt="" onClick={() => props.showSideBar()} className="btn" src={xIcon} />
                 </div>
                 <div className="sidebar-selects" >
                 <p>Country</p>
-                    <select onClick={(e) => {handleSelect(e)}} className={`sidebar-select ${theme}-mode`} >
+                    <select onClick={(e) => {handleSelectCountry(e)}}  className={`sidebar-select ${theme}-mode`} >
                         <option value='no select' >Select a country</option>
                         <option value='2'>Sele sd ountry</option>
                         <option>Sele asd country</option>
@@ -36,9 +53,9 @@ function SideBar(props){
                         <option>Select a country</option>
                     </select>
                     <p className="select-city">City</p>
-                    <select className={`sidebar-select ${theme}-mode`} ref={selectRef} disabled={true} >
-                        <option>Select a city</option>
-                        <option>Select a country</option>
+                    <select onClick={(e) => handleSelectCity(e)} className={`sidebar-select ${theme}-mode`} disabled={isDisableSelectCountry} >
+                        <option value='no select' >Select a city</option>
+                        <option onClick={(e) => console.log('hello')}  value='3'>Selectsss country</option>
                         <option>Select a country</option>
                         <option>Select a country</option>
                         <option>Select a country</option>
@@ -46,8 +63,20 @@ function SideBar(props){
                     </select>
                 </div>
                 <div className="sidebar-footer" >
-                    <button  className={`btn ${theme}-mode`}><img alt="" className="btn-img" src={cancelIcon}/>Cancel</button>
-                    <button  className={`btn btn-save`}><img alt="" className="btn-img" src={saveIcon} />Save</button>
+                    <button 
+                        className={`btn ${theme}-mode`}
+                        onClick={() => props.showSideBar()}
+                            >
+                            <img alt="" className="btn-img" src={cancelIcon} />
+                        Cancel
+                    </button>
+
+                    <button 
+                        className={`btn btn-save`} 
+                        disabled={isDisableSelectCity} >
+                            <img alt="" disabled={isDisableSelectCity} className="btn-img" src={saveIcon} />
+                        Save
+                    </button>
                 </div>
             </div>
         </div>
@@ -55,7 +84,8 @@ function SideBar(props){
 }
 const mapStateToProps = state =>{
     return {
-        isToggle:state.isToggle
+        isToggle:state.isToggle,
+        isShow:state.isShow
     }
 }
-export default connect(mapStateToProps)(SideBar)
+export default connect(mapStateToProps, {showSideBar})(SideBar)
