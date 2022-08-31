2fd04776306eb2844da9a62eba35dd050bc599a5
yakubalkis <yakupalkiis@gmail.com>
2022-09-01 00:03:26 +0300

    Taken part Usefect for handlng savbtn from Sidebar

--- src/components/WeatherCarts/Cart.js
+++ src/components/WeatherCarts/Cart.js
@@ -1,25 +1,28 @@
+import React from 'react'
+import iconSun from '../img/icon-sun.png'
 export default  function Cart(){
     return(
         <div className="cart">
            <p className="cart-day">Wednesday</p>
-           <p className="cart-day">31</p>
-           <img className="cart-icon" alt="" />
+           <p className="cart-day-number">31</p>
+           <img className="cart-icon" alt="" src={iconSun} />
            <p className="cart-city">Leira<span className="cart-city-country">PT</span></p>
            <p className="cart-weather-state">Scattered clouds</p>
            <div className="cart-weather-info-div">
-            <div className="cart-weather-info">
-                <p>Current Temp.</p>
-                <p>23°C</p>
-            </div>
-            <div className="cart-weather-info">
-                <p>Feels Like</p>
-                <p>23°C</p>
-            </div>
-            <div className="cart-weather-info">
-                <p>Humidity</p>
-                <p>71%</p>
-            </div>
+                <div className="cart-weather-info">
+                    <p className='cart-weather-temp-info'>Current Temp.</p>
+                    <p className='temp'>23°C</p>
+                </div>
+                <div className="cart-weather-info">
+                    <p className='cart-weather-temp-info'>Feels Like</p>
+                    <p className='temp'>23°C</p>
+                </div>
+                <div className="cart-weather-info">
+                    <p className='cart-weather-temp-info'>Humidity</p>
+                    <p className='temp'>71%</p>
+                </div>
            </div>
+           <img alt='' />
         </div>
     )
 }
