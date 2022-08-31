a7ed4a11fa3f28f49488792c15369ec2e19a3a24
yakubalkis <yakupalkiis@gmail.com>
2022-08-31 11:32:56 +0300

    Added action getCity.

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,19 @@
+import axios from "axios"
+
 export const toggle = () => dispatch => {
     dispatch({type: 'TOGGLE'})
 }
 export const showSideBar = () => dispatch => {
     dispatch({type: 'SHOW_SIDEBAR'})
 }
+export const getCountries = () => dispatch => {
+    axios
+    .get("https://countriesnow.space/api/v0.1/countries")
+    .then(response => dispatch({type: 'GET_COUNTRIES_SUCCESS', payload:response.data}))
+}
+export const getCountry = (country) => dispatch => {
+    dispatch({type: 'GET_COUNTRY', payload:country})
+}
+export const getCity = (city) => dispatch => {
+    dispatch({type: 'GET_CITY', payload:city})
+} 
