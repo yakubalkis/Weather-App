4304a79f9ef08009e2c8f664989137d62c951194
yakubalkis <yakupalkiis@gmail.com>
2022-08-31 11:33:14 +0300

    Added reducer getCity

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,14 +1,23 @@
 const INITIAL_STATE = {
     isToggle: false,
-    isShow:false
+    isShow:false,
+    countries:[],
+    selectedCountry:'',
+    selectedCity:''
 }
 
 export const reducer = ( state = INITIAL_STATE, action ) => {
     switch(action.type){
         case 'TOGGLE':
-            return{...state, isToggle:!state.isToggle}
+            return {...state, isToggle:!state.isToggle}
         case 'SHOW_SIDEBAR':
-            return{...state, isShow:!state.isShow}
+            return {...state, isShow:!state.isShow}
+        case 'GET_COUNTRIES_SUCCESS':
+            return {...state, countries:action.payload}
+        case 'GET_COUNTRY':
+            return {...state, selectedCountry:action.payload}
+        case 'GET_CITY':
+            return {...state, selectedCity:action.payload}
         default:
             return state
     }
