94c4a6ff3be7123900dbac14896ad46b3441fa50
yakubalkis <yakupalkiis@gmail.com>
2022-09-02 15:04:47 +0300

    Added reducer to remove city.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -5,7 +5,10 @@ const INITIAL_STATE = {
     selectedCountry:'',
     selectedCity:'',
     selectedCities:['Berlin'],
-    infoOfSelectedCity:[]
+    infoOfSelectedCities:[],
+    allWeatherForecasts:[],
+    isTakenPositionFromApi:false,
+    removedCities:[]
 }
 
 export const reducer = ( state = INITIAL_STATE, action ) => {
@@ -16,16 +19,23 @@ export const reducer = ( state = INITIAL_STATE, action ) => {
             return {...state, isShow:true}
         case 'HIDE_SIDEBAR':
             return {...state, isShow:false}
+     
         case 'GET_COUNTRIES_SUCCESS':
             return {...state, countries:action.payload}
+        case 'GET_POSITION_OF_CITY_START':
+            return {...state, isTakenPositionFromApi:false}
         case 'GET_POSITION_OF_CITY':
-            return {...state, infoOfSelectedCity:action.payload}
+            return {...state, infoOfSelectedCities:[...state.infoOfSelectedCities,action.payload], isTakenPositionFromApi:true}
+        case 'GET_WEATHER_FORECAST':
+            return {...state, allWeatherForecasts:[...state.allWeatherForecasts, action.payload]}
         case 'GET_COUNTRY':
             return {...state, selectedCountry:action.payload}
         case 'GET_CITY':
             return {...state, selectedCity:action.payload}
         case 'ADD_CITY':
-            return{...state, selectedCities:[...state.selectedCities, action.payload]}
+            return {...state, selectedCities:[...state.selectedCities, action.payload]}
+        case 'REMOVE_CITY':
+            return {...state, removedCities:[...state.removedCities, action.payload] }
         default:
             return state
     }
