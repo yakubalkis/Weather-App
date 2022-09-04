4992b26d58e5ecdc4e53a5c56dc649528e8c5085
yakubalkis <yakupalkiis@gmail.com>
2022-09-04 20:47:46 +0300

    Removed props  from component.

--- src/components/Main-homepage/Shadow.js
+++ src/components/Main-homepage/Shadow.js
@@ -1,14 +1,7 @@
-import React from "react"
-import { connect } from "react-redux"
-
-function Shadow(props){
+export default function Shadow(){
     return (
-        <div className="shadow" style={{display: props.isShow ? 'block':'none'}}  ></div>
+        <>
+        <div className="shadow" ></div>
+        </>
     )
 }
-const mapStateToProps = state => {
-    return {
-        isShow:state.isShow
-    }
-}
-export default connect(mapStateToProps)(Shadow)
