3aab95f0592cd57db05c4bf56a098a2cb61d610a
yakubalkis <yakupalkiis@gmail.com>
2022-09-04 15:15:32 +0300

    added useEffect for outside click process

--- src/components/WeatherCarts/CartMenu.js
+++ src/components/WeatherCarts/CartMenu.js
@@ -1,24 +1,35 @@
-import React,{useState} from "react";
+import React,{useState, useEffect, useRef} from "react";
 import { connect } from "react-redux";
-import { getRemovedCityId, getRemovedCityName,deleteRemovedCityData } from "../../redux/actions";
+import { getRemovedCityId, getRemovedCityName,getDataOfWillBeRemovedCity,showSidebarDeleteCity } from "../../redux/actions";
 import iconMenu from '../img/icon-menu.png'
 
 function CartMenu(props){
 
+    const menuRef=useRef()
     const [isShowMenu,setIsShowMenu] = useState(false)
+
+    useEffect(()  => {
+        document.addEventListener("mousedown", (event) =>  {
+            if(!menuRef.current?.contains(event.target))
+                setIsShowMenu(false)
+        })
+    })
+    
     const style = {
         display: isShowMenu ? 'block':'none'
     }
     const theme = props.isToggle ? 'dark' : 'light'
+    
     return (
         <>
-           <img alt='' src={iconMenu} className='icon-menu' onClick={() => {setIsShowMenu(prevState => !prevState)}} />
+           <img alt='' src={iconMenu} className='icon-menu' onClick={() => {setIsShowMenu(true)}} />
            <div className='cart-menu'  >
-             <button style={style} className={`cart-menu-button button-top ${theme}-modeSidebar`}>View Weather</button>
+             <button style={style} className={`cart-menu-button button-top ${theme}-modeSidebar`} ref={menuRef} >View Weather</button>
              <button 
-                    style={style} 
+                    style={style}
+                    ref={menuRef} 
                     className={`cart-menu-button button-bottom ${theme}-modeSidebar`}
-                    onClick={() => {props.deleteRemovedCityData(props.idOfCity, props.cityName, props.index)}}
+                    onClick={() => {props.getDataOfWillBeRemovedCity(props.idOfCity, props.cityName, props.index) ; props.showSidebarDeleteCity() ; props.getRemovedCityName(props.cityName)}}
                     >
                         Remove City
              </button>
@@ -31,4 +42,4 @@ const mapStateToProps = state => {
         isToggle:state.isToggle
     }
 }
-export default connect(mapStateToProps, {getRemovedCityId, getRemovedCityName,deleteRemovedCityData})(CartMenu)
+export default connect(mapStateToProps, {getRemovedCityId, getRemovedCityName,getDataOfWillBeRemovedCity,showSidebarDeleteCity})(CartMenu)
