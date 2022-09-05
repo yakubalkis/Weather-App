9ded1d0808dbe563282b8f45c4205dcdd515bc38
yakubalkis <yakupalkiis@gmail.com>
2022-09-05 10:46:43 +0300

    Added router.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { legacy_createStore as createStore, applyMiddleware} from 'redux';
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { reducer } from './redux/reducers'
+import {BrowserRouter as Router} from 'react-router-dom'
+import './index.css';
+import App from './App';
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
                 <Provider store={store} >
-                    <App/>
+                    <Router>
+                        <App/>
+                    </Router>
                 </Provider>   
                     , document.getElementById('root'))
