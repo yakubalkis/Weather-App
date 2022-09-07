56f68993f7ef7ac1a77ea135e16c26a8a8344355
yakubalkis <yakupalkiis@gmail.com>
2022-09-07 15:22:03 +0300

    Added Link to header to redirecy homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { toggle} from "../../redux/actions";
 import darkIcon from '../img/dark-icon.png'
 import lightIcon from '../img/light-icon.png'
-import { connect } from "react-redux";
-import { toggle } from "../../redux/actions";
 
 function Header(props){
 
@@ -10,8 +11,8 @@ function Header(props){
     const btnIcon = props.isToggle ? lightIcon: darkIcon
 
     return (
-        <header className={`header ${theme}-mode`}>
-            <h2 className={`${theme}-mode`}>Weather App</h2>
+        <header className={`header ${theme}-mode`} >
+            <Link to='/' style={{textDecoration:'none'}}><h2 className={`${theme}-mode`}>Weather App</h2></Link>
             <button onClick={() => props.toggle()} className={`btn ${theme}-modeForBtnMode`}><img alt="" src={btnIcon} /></button>
         </header>
     )
