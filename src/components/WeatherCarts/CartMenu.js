79783077744149ff810e2892196cdcdef71c6a65
yakubalkis <yakupalkiis@gmail.com>
2022-09-07 15:03:06 +0300

    Added Link for routing operations

--- src/components/WeatherCarts/CartMenu.js
+++ src/components/WeatherCarts/CartMenu.js
@@ -1,13 +1,20 @@
 import React,{useState, useEffect, useRef} from "react";
 import { connect } from "react-redux";
-import { getRemovedCityId, getRemovedCityName,getDataOfWillBeRemovedCity,showSidebarDeleteCity } from "../../redux/actions";
+import { getDataOfWillBeRemovedCity,showSidebarDeleteCity,getViewedCityName,isUserComeBackHomePage } from "../../redux/actions";
+import { Link } from "react-router-dom";
 import iconMenu from '../img/icon-menu.png'
 
 function CartMenu(props){
 
-    const menuRef=useRef()
-    const [isShowMenu,setIsShowMenu] = useState(false)
 
+    const [isShowMenu,setIsShowMenu] = useState(false)
+    const style = {
+        display: isShowMenu ? 'block':'none'
+    }
+    const theme = props.isToggle ? 'dark' : 'light'
+
+    
+    const menuRef=useRef()
     useEffect(()  => {
         document.addEventListener("mousedown", (event) =>  {
             if(!menuRef.current?.contains(event.target))
@@ -15,31 +22,37 @@ function CartMenu(props){
         })
     })
     
-    const style = {
-        display: isShowMenu ? 'block':'none'
-    }
-    const theme = props.isToggle ? 'dark' : 'light'
     
     return (
         <>
-           <img alt='' src={iconMenu} className='icon-menu' onClick={() => {setIsShowMenu(true)}} />
-           <div className='cart-menu'  >
-             <button style={style} className={`cart-menu-button button-top ${theme}-modeSidebar`} ref={menuRef} >View Weather</button>
+           <img alt='' src={iconMenu} className='icon-menu'  onClick={() => {setIsShowMenu(true);props.getViewedCityName(props.cityName)}} />
+           <div className='cart-menu' ref={menuRef} style={style}   >
+            <Link to={`/${props.viewedCityName}`} style={{textDecoration:"none"}}>
+                <button 
+                        style={style} 
+                        className={`cart-menu-button button-top ${theme}-modeSidebar`} 
+                        ref={menuRef}
+                        onClick={() =>{props.getDataOfWillBeRemovedCity(props.idOfCity, props.cityName, props.index); props.isUserComeBackHomePage()}} 
+                        >
+                            View Weather
+                </button>
+            </Link>
              <button 
                     style={style}
-                    ref={menuRef} 
                     className={`cart-menu-button button-bottom ${theme}-modeSidebar`}
-                    onClick={() => {props.getDataOfWillBeRemovedCity(props.idOfCity, props.cityName, props.index) ; props.showSidebarDeleteCity() ; props.getRemovedCityName(props.cityName)}}
+                    ref={menuRef} 
+                    onClick={() => {props.getDataOfWillBeRemovedCity(props.idOfCity, props.cityName, props.index) ; props.showSidebarDeleteCity()}}
                     >
                         Remove City
              </button>
            </div>
         </>
     )
-}
+} 
 const mapStateToProps = state => {
     return {
-        isToggle:state.isToggle
+        isToggle:state.isToggle,
+        viewedCityName:state.viewedCityName
     }
 }
-export default connect(mapStateToProps, {getRemovedCityId, getRemovedCityName,getDataOfWillBeRemovedCity,showSidebarDeleteCity})(CartMenu)
+export default connect(mapStateToProps, {getDataOfWillBeRemovedCity,showSidebarDeleteCity,getViewedCityName,isUserComeBackHomePage})(CartMenu)
