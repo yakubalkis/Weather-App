6487ecdefb0eb596874f7410fb979d0ff3d737c0
yakubalkis <yakupalkiis@gmail.com>
2022-09-07 15:04:19 +0300

    Taken Cart compoenent for reusable.

--- src/components/pages/CityWeatherWeekly.js
+++ src/components/pages/CityWeatherWeekly.js
@@ -1,19 +1,41 @@
 import React from "react"
 import { connect } from "react-redux"
 import Cart from "../WeatherCarts/Cart"
+import DetailedSmallCart from "./DetailedSmallCart"
 
-function CityWeatherWeekly(){
+function CityWeatherWeekly(props){
 
+    const firstWeatherCart= props.allWeatherForecasts.map((city,i) => {
+   
+        if(props.data[2] === i){
+            return <Cart 
+                    key={i}
+                    idOfCity={city.id}
+                    index={i}
+                    city={props.data[1]}
+                    country={city.sys.country}
+                    weatherState={city.weather[0].description}
+                    currentTemp={city.main.temp}
+                    feelsLike={city.main.feels_like}
+                    humidity={city.main.humidity}
+                />
+        }
+        else return false
+    })
     return(
             <div className="carts">
-                HELLLOOO
+                {firstWeatherCart}
+                <div className="carts-6days">
+                   <DetailedSmallCart/>
+                </div>
             </div>
     )
     
 }
 const mapStateToProps = state => {
     return {
-        allWeatherForecasts:state.allWeatherForecasts
+        allWeatherForecasts:state.allWeatherForecasts,
+        data:state.data
     }
 }
 export default connect(mapStateToProps)(CityWeatherWeekly)
