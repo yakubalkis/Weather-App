6d8a26cb95b7a7b1d652c9e874b764e49faf3596
yakubalkis <yakupalkiis@gmail.com>
2022-09-07 21:39:54 +0300

    Added animation for small weather carts

--- src/components/pages/CityWeatherWeekly.js
+++ src/components/pages/CityWeatherWeekly.js
@@ -1,33 +1,48 @@
-import React from "react"
+import React,{useState, useRef, useEffect} from "react"
 import { connect } from "react-redux"
+import { motion } from "framer-motion"
 import Cart from "../WeatherCarts/Cart"
 import DetailedSmallCart from "./DetailedSmallCart"
 
 function CityWeatherWeekly(props){
 
+    const [width, setWidth] = useState(0)
+    const carouselRef = useRef()
+    useEffect(() =>{
+        setWidth(carouselRef.current.scrollWidth - carouselRef.current.offsetWidth)
+    },[])
     const firstWeatherCart= props.allWeatherForecasts.map((city,i) => {
-   
         if(props.data[2] === i){
             return <Cart 
-                    key={i}
-                    idOfCity={city.id}
-                    index={i}
-                    city={props.data[1]}
-                    country={city.sys.country}
-                    weatherState={city.weather[0].description}
-                    currentTemp={city.main.temp}
-                    feelsLike={city.main.feels_like}
-                    humidity={city.main.humidity}
-                />
+                        key={i}
+                        idOfCity={city.id}
+                        index={i}
+                        city={props.data[1]}
+                        country={city.sys.country}
+                        weatherState={city.weather[0].description}
+                        currentTemp={city.main.temp}
+                        feelsLike={city.main.feels_like}
+                        humidity={city.main.humidity}
+                   />
         }
         else return false
     })
+
     return(
             <div className="carts">
                 {firstWeatherCart}
-                <div className="carts-6days">
-                   <DetailedSmallCart/>
-                </div>
+                <motion.div ref={carouselRef} className="carousel">
+                    <motion.div drag="x" dragConstraints={{right:0,left:-width}} className="inner-carousel">
+                        <motion.div className="item"><DetailedSmallCart/></motion.div> 
+                        <motion.div className="item"><DetailedSmallCart/></motion.div>
+                        <motion.div className="item"><DetailedSmallCart/></motion.div>
+                        <motion.div className="item"><DetailedSmallCart/></motion.div>
+                        <motion.div className="item"><DetailedSmallCart/></motion.div>
+                        <motion.div className="item"><DetailedSmallCart/></motion.div>
+                        <motion.div className="item"><DetailedSmallCart/></motion.div>
+                    </motion.div>
+                   
+                </motion.div>
             </div>
     )
     
