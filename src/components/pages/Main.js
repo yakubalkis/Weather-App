c3327433df5126adf35499670ee52111a8cebe57
yakubalkis <yakupalkiis@gmail.com>
2022-09-07 15:02:15 +0300

    Passed  props for increasing functionality

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
-import { showSidebarDeleteCity } from "../../redux/actions";
+import { showSidebarDeleteCity,getDataOfWillBeRemovedCity } from "../../redux/actions";
+import CityWeatherWeekly from "./CityWeatherWeekly";
+
 import SideBarDeleteCity from "../WeatherCarts/SideBarDeleteCity";
 
 function Main(props){
 
     const theme = props.isToggle ? 'dark':'light'
-
+    console.log(props.allWeatherForecasts)
+    console.log(props.infoOfSelectedCities)
+   
     return (
             <>
                 <main className={"main"} >
                     <div className={"main-elements"}>
                         <h1>{props.viewedCityName}</h1>
                         <h2>Weather for the next <b>7 days</b></h2>
-                        <button onClick={() => {props.showSidebarDeleteCity()}} className={`${theme}-modeForBtnAdd remove-btn btn`} >Remove City</button>
+                        <button onClick={() => {props.showSidebarDeleteCity()}} 
+                                className={`${theme}-modeForBtnAdd remove-btn btn`} >Remove City
+                        </button>
                     </div>
                 </main>
+                <CityWeatherWeekly/>
                 <SideBarDeleteCity />
             </>
     )
@@ -24,7 +31,9 @@ function Main(props){
 const mapStateToProps = state => {
     return {
         isToggle:state.isToggle,
-        viewedCityName:state.viewedCityName
+        viewedCityName:state.viewedCityName,
+        allWeatherForecasts:state.allWeatherForecasts,
+        infoOfSelectedCities:state.infoOfSelectedCities
     }
 }
-export default connect(mapStateToProps, {showSidebarDeleteCity})(Main)
+export default connect(mapStateToProps, {showSidebarDeleteCity,getDataOfWillBeRemovedCity})(Main)
