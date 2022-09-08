2625a9bcc522cbed48a0b20a3cbcb31bd83794f7
yakubalkis <yakupalkiis@gmail.com>
2022-09-08 19:01:20 +0300

    Added and called funcs to get minmax  temp of days

--- src/components/pages/CityWeatherWeekly.js
+++ src/components/pages/CityWeatherWeekly.js
@@ -2,18 +2,38 @@ import React,{useState, useRef, useEffect} from "react"
 import { connect } from "react-redux"
 import { motion } from "framer-motion"
 //import Cart from "../WeatherCarts/Cart"
+import getDays from "../CustomFunctions/getDays"
+import getMaxMinTemp from "../CustomFunctions/getMaxMinTemp"
 import FirstWeatherCart from "./firstWeatherCart"
 import DetailedSmallCart from "./detailedSmallCart"
 
 function CityWeatherWeekly(props){
 
     const [width, setWidth] = useState(0)
+    let maxMinTemps;
+    const currentDay = getDays()[0]
     const carouselRef = useRef()
     useEffect(() =>{
         setWidth(carouselRef.current.scrollWidth - carouselRef.current.offsetWidth)
-    },[])
+    },[]) 
+    useEffect(() => {
+        if(props.weeklyWeatherForecast.length !== 0){
+            maxMinTemps = getMaxMinTemp(props.weeklyWeatherForecast)
+            console.log(maxMinTemps)
+        }
+    },[props.weeklyWeatherForecast])
     
+
+    // const detailedSmallCarts = props.weeklyWeatherForecast ?  props.weeklyWeatherForecast.map((item,i) => {
+    //     if(currentDay !== item.dt_txt[8]+item.dt_txt[9]){
+    //         i=i+8
+    //         console.log('hello')
+    //     }
+       
+    // }) :  false
+   
     console.log(props.weeklyWeatherForecast)
+
     return(
             <div className="carts">
                 <FirstWeatherCart />
