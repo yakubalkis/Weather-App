5fef441e13ecb9ca27cc606fa3a1c6ec308cb9c8
yakubalkis <yakupalkiis@gmail.com>
2022-09-08 18:39:54 +0300

    Fixed function.

--- src/components/CustomFunctions/getMaxMinTemp.js
+++ src/components/CustomFunctions/getMaxMinTemp.js
@@ -3,29 +3,29 @@ import getDays from "./getDays"
 export default function getMaxMinTemp(weeklyWeatherForecast){
     const currentDay = getDays()[0]
     const temps = []
-    
     const daysWeather = weeklyWeatherForecast.filter((item) => item.dt_txt[8]+item.dt_txt[9] !== currentDay)
     let maxTemp=daysWeather[0].main.temp_max
     let minTemp=daysWeather[0].main.temp_min
+
     for(let i = 1; i < daysWeather.length; i++){
         
-    
         if(daysWeather[i].dt_txt[8]+daysWeather[i].dt_txt[9] === daysWeather[i-1].dt_txt[8]+daysWeather[i-1].dt_txt[9]){ // check whether is same day
-            maxTemp= daysWeather[i].main.temp_max >= daysWeather[i-1].main.temp_max ? maxTemp >= daysWeather[i].main.temp_max ? maxTemp : daysWeather[i].main.temp_max : maxTemp >= daysWeather[i-1].main.temp_max ? maxTemp : daysWeather[i-1].main.temp_max
-            minTemp= daysWeather[i].main.temp_min <= daysWeather[i-1].main.temp_min ? minTemp <= daysWeather[i].main.temp_min ? minTemp : daysWeather[i].main.temp_min : minTemp <= daysWeather[i-1].main.temp_min ? minTemp : daysWeather[i-1].main.temp_min
+            if(i === daysWeather.length-1){
+                maxTemp= daysWeather[i].main.temp_max >= daysWeather[i-1].main.temp_max ? maxTemp >= daysWeather[i].main.temp_max ? maxTemp : daysWeather[i].main.temp_max : maxTemp >= daysWeather[i-1].main.temp_max ? maxTemp : daysWeather[i-1].main.temp_max
+                minTemp= daysWeather[i].main.temp_min <= daysWeather[i-1].main.temp_min ? minTemp <= daysWeather[i].main.temp_min ? minTemp : daysWeather[i].main.temp_min : minTemp <= daysWeather[i-1].main.temp_min ? minTemp : daysWeather[i-1].main.temp_min
+                temps.push({maxTemp,minTemp})
+            }
+            else{maxTemp= daysWeather[i].main.temp_max >= daysWeather[i-1].main.temp_max ? maxTemp >= daysWeather[i].main.temp_max ? maxTemp : daysWeather[i].main.temp_max : maxTemp >= daysWeather[i-1].main.temp_max ? maxTemp : daysWeather[i-1].main.temp_max
+                 minTemp= daysWeather[i].main.temp_min <= daysWeather[i-1].main.temp_min ? minTemp <= daysWeather[i].main.temp_min ? minTemp : daysWeather[i].main.temp_min : minTemp <= daysWeather[i-1].main.temp_min ? minTemp : daysWeather[i-1].main.temp_min
+            }
         }
         else if(daysWeather[i].dt_txt[8]+daysWeather[i].dt_txt[9] !== daysWeather[i-1].dt_txt[8]+daysWeather[i-1].dt_txt[9]){
             temps.push({maxTemp,minTemp})
             maxTemp=daysWeather[i].main.temp_max
             minTemp=daysWeather[i].main.temp_min
         }
-
     } 
-    console.log(daysWeather)
-   
-    console.log(temps)
+   return temps
 
-    
-    
 }
 
