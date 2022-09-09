553587dbd8b59b51a7d785932734a8add032061f
yakubalkis <yakupalkiis@gmail.com>
2022-09-09 12:14:05 +0300

    Added day variable to return also day name

--- src/components/CustomFunctions/getMaxMinTemp.js
+++ src/components/CustomFunctions/getMaxMinTemp.js
@@ -6,6 +6,7 @@ export default function getMaxMinTemp(weeklyWeatherForecast){
     const daysWeather = weeklyWeatherForecast.filter((item) => item.dt_txt[8]+item.dt_txt[9] !== currentDay)
     let maxTemp=daysWeather[0].main.temp_max
     let minTemp=daysWeather[0].main.temp_min
+    let day = ''
 
     for(let i = 1; i < daysWeather.length; i++){
         
@@ -13,14 +14,17 @@ export default function getMaxMinTemp(weeklyWeatherForecast){
             if(i === daysWeather.length-1){
                 maxTemp= daysWeather[i].main.temp_max >= daysWeather[i-1].main.temp_max ? maxTemp >= daysWeather[i].main.temp_max ? maxTemp : daysWeather[i].main.temp_max : maxTemp >= daysWeather[i-1].main.temp_max ? maxTemp : daysWeather[i-1].main.temp_max
                 minTemp= daysWeather[i].main.temp_min <= daysWeather[i-1].main.temp_min ? minTemp <= daysWeather[i].main.temp_min ? minTemp : daysWeather[i].main.temp_min : minTemp <= daysWeather[i-1].main.temp_min ? minTemp : daysWeather[i-1].main.temp_min
-                temps.push({maxTemp,minTemp})
+                day = daysWeather[i].dt_txt[8]+daysWeather[i].dt_txt[9]
+                temps.push({maxTemp,minTemp,day})
             }
             else{maxTemp= daysWeather[i].main.temp_max >= daysWeather[i-1].main.temp_max ? maxTemp >= daysWeather[i].main.temp_max ? maxTemp : daysWeather[i].main.temp_max : maxTemp >= daysWeather[i-1].main.temp_max ? maxTemp : daysWeather[i-1].main.temp_max
                  minTemp= daysWeather[i].main.temp_min <= daysWeather[i-1].main.temp_min ? minTemp <= daysWeather[i].main.temp_min ? minTemp : daysWeather[i].main.temp_min : minTemp <= daysWeather[i-1].main.temp_min ? minTemp : daysWeather[i-1].main.temp_min
+                 
             }
         }
         else if(daysWeather[i].dt_txt[8]+daysWeather[i].dt_txt[9] !== daysWeather[i-1].dt_txt[8]+daysWeather[i-1].dt_txt[9]){
-            temps.push({maxTemp,minTemp})
+            day = daysWeather[i-1].dt_txt[8]+daysWeather[i-1].dt_txt[9]
+            temps.push({maxTemp,minTemp,day})
             maxTemp=daysWeather[i].main.temp_max
             minTemp=daysWeather[i].main.temp_min
         }
