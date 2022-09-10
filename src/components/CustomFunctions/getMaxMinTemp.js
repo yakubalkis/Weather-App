a198505c71d0bfb04b002e3f0b969b6965a39e31
yakubalkis <yakupalkiis@gmail.com>
2022-09-10 11:25:40 +0300

    Fixed bug

--- src/components/CustomFunctions/getMaxMinTemp.js
+++ src/components/CustomFunctions/getMaxMinTemp.js
@@ -3,11 +3,11 @@ import getDays from "./getDays"
 export default function getMaxMinTemp(weeklyWeatherForecast){
     const currentDay = getDays()[0]
     const temps = []
-    const daysWeather = weeklyWeatherForecast.filter((item) => item.dt_txt[8]+item.dt_txt[9] !== currentDay)
+    const daysWeather = weeklyWeatherForecast.filter((item) => Number(item.dt_txt[8]+item.dt_txt[9]) !== currentDay)
     let maxTemp=daysWeather[0].main.temp_max
     let minTemp=daysWeather[0].main.temp_min
     let day = ''
-
+    
     for(let i = 1; i < daysWeather.length; i++){
         
         if(daysWeather[i].dt_txt[8]+daysWeather[i].dt_txt[9] === daysWeather[i-1].dt_txt[8]+daysWeather[i-1].dt_txt[9]){ // check whether is same day
