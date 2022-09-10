30a1e898fb8242d844feabbd0a1cb28f1ae34c96
yakubalkis <yakupalkiis@gmail.com>
2022-09-10 13:34:12 +0300

    Added hover text for img element

--- src/components/pages/DetailedSmallCart.js
+++ src/components/pages/DetailedSmallCart.js
@@ -7,7 +7,7 @@ export default function DetailedSmallCart(props){
         <div className="cart cart-small">
             <p className="cart-day">{getDaysNames(props.index)}</p>
             <p className="cart-day-number">{props.day}</p>
-            <img className="cart-icon" alt="" src={getWeatherIcon(props.description, props.currentTemp)} />
+            <div className="descriptionText" data-hover={props.description}><img className="cart-icon" alt="" src={getWeatherIcon(props.description, props.currentTemp)} /></div>
             <div className="cart-weather-info-div">
                     <p className='temp tempText' data-hover="Max Temperature" >{Math.round(props.maxTemp)}°C</p>
                     <p className='temp tempText' data-hover="Min Temperature" style={{opacity:'0.7'}}>{Math.round(props.minTemp)}°C</p>
