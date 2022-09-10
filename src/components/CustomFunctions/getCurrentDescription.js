bf50ac4a95c09a1e0ec1915d4a4f15983ed313c3
yakubalkis <yakupalkiis@gmail.com>
2022-09-10 11:26:55 +0300

    Fixed bug

--- src/components/CustomFunctions/getCurrentDescription.js
+++ src/components/CustomFunctions/getCurrentDescription.js
@@ -4,15 +4,16 @@ export default function getCurrentDescription(weeklyWeatherForecast){
 
     const currentDay = getDays()[0]
     const currentHour = getDays()[2]
-    const daysWeather = weeklyWeatherForecast.filter((item) => item.dt_txt[8]+item.dt_txt[9] !== currentDay)
+    const daysWeather = weeklyWeatherForecast.filter((item) => Number(item.dt_txt[8]+item.dt_txt[9]) !== currentDay)
     const descriptionsAndTemps = []
-    console.log(daysWeather)
+
     for(let i = 1; i < daysWeather.length; i++){
         const hour1 = Number(daysWeather[i].dt_txt[11]+daysWeather[i].dt_txt[12])
         const hour2 = Number(daysWeather[i-1].dt_txt[11]+daysWeather[i-1].dt_txt[12])
+        
         if(hour1 >= currentHour && currentHour >= hour2 ){
-            const description = hour1 - currentHour > currentHour - hour2 ? daysWeather[i].weather[0].description : daysWeather[i-1].weather[0].description
-            const currentTemp = hour1 - currentHour > currentHour - hour2 ? daysWeather[i].main.temp : daysWeather[i-1].main.temp
+            const description = hour1 - currentHour > currentHour - hour2 ? daysWeather[i-1].weather[0].description : daysWeather[i].weather[0].description
+            const currentTemp = hour1 - currentHour > currentHour - hour2 ? daysWeather[i-1].main.temp : daysWeather[i].main.temp
             descriptionsAndTemps.push({description,currentTemp})
         }
         else if(i === daysWeather.length -1){
