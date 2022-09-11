24d92a67d4d094d1b670edd4f8c75eda0504e71a
yakubalkis <yakupalkiis@gmail.com>
2022-09-11 23:39:45 +0300

    Added function to display component for popup

--- src/components/Main-homepage/Shadow.js
+++ src/components/Main-homepage/Shadow.js
@@ -1,13 +1,13 @@
 import { connect } from "react-redux"
-import { hideSidebarAddCity, hideSidebarDeleteCity } from "../../redux/actions"
+import { hideSidebarAddCity, hideSidebarDeleteCity,hidePopup } from "../../redux/actions"
 function Shadow(props){
     return (
         <>
-        <div onClick={() => {props.hideSidebarDeleteCity();props.hideSidebarAddCity()}} className="shadow" ></div>
+        <div onClick={() => {props.hideSidebarDeleteCity();props.hideSidebarAddCity();props.hidePopup()}} className="shadow" ></div>
         </>
     )
 }
 const mapStateToProps = state => {
     return {}
 }
-export default connect(mapStateToProps, {hideSidebarAddCity,hideSidebarDeleteCity})(Shadow)
+export default connect(mapStateToProps, {hideSidebarAddCity,hideSidebarDeleteCity, hidePopup})(Shadow)
