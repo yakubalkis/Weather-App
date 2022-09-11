15e26603becf1438f0988c210889f2e929945fab
yakubalkis <yakupalkiis@gmail.com>
2022-09-11 23:42:55 +0300

    Added Popup component

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,29 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
 import { showSidebarDeleteCity,getDataOfWillBeRemovedCity } from "../../redux/actions";
+import Popup from "./Popup";
 import CityWeatherWeekly from "./CityWeatherWeekly";
+
 
 import SideBarDeleteCity from "../WeatherCarts/SideBarDeleteCity";
 
 function Main(props){
 
     const theme = props.isToggle ? 'dark':'light'
-    console.log(props.allWeatherForecasts)
-    console.log(props.infoOfSelectedCities)
+    
    
     return (
             <>
                 <main className={"main"} >
                     <div className={"main-elements"}>
                         <h1>{props.viewedCityName}</h1>
-                        <h2>Weather for the next <b>7 days</b></h2>
+                        <h2>Weather for the next <b>6 days</b></h2>
                         <button onClick={() => {props.showSidebarDeleteCity()}} 
                                 className={`${theme}-modeForBtnAdd remove-btn btn`} >Remove City
                         </button>
                     </div>
-                </main>
+                </main> 
                 <CityWeatherWeekly/>
                 <SideBarDeleteCity />
+                <Popup/>
             </>
     )
     
