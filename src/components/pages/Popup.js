584a5d0fa16035de3ebcd44e2a7c2a007e2d2727
yakubalkis <yakupalkiis@gmail.com>
2022-09-11 23:37:09 +0300

    Added functionality for showing popup

--- src/components/pages/Popup.js
+++ src/components/pages/Popup.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import Cart from "../WeatherCarts/Cart";
+import { hidePopup } from "../../redux/actions";
+import Shadow from "../Main-homepage/Shadow";
 import xIconDark from '../img/x-iconDark.png'
 import xIconLight from '../img/x-iconLight.png'
 import windIconDark from '../img/icon-windDark.png'
@@ -9,53 +10,55 @@ import cloudsIconDark from '../img/icon-cloudsDark.png'
 
 function Popup(props){
 
-    useEffect(() => {
-        console.log(props.currentWeatherOfDay)
-    },[props.currentWeatherOfDay]) 
+    
     
     const xIcon = props.isToggle ? xIconDark : xIconLight
     const theme = props.isToggle ? 'dark' : 'light'
 
     return (
-        <div className={`popup ${theme}-modeSidebar`}>
-            <div className="popup-header">
-                <p>Weather Info from <b>September 12th</b></p>
-                <img alt=""  className="btn" src={xIcon} />
-            </div>
-            <div className="popup-main">
-                <div className="cart popup-cart"></div>
-                <div className="popup-info">
-                    <div className="popup-info-part">
-                        <img  alt=""src={windIconDark} />
-                        <div className="popup-info-part-text" >
-                            <h3>Wind</h3>
-                            <h4><b>Velocity: </b>2 km/h </h4>
-                            <h4><b>Direction: </b>333°</h4>
+        <>
+            <div className={`popup ${theme}-modeSidebar`} style={{display: props.isShowPopup ? 'block':'none'}}>
+                <div className="popup-header">
+                    <p>Weather Info from <b>September 12th</b></p>
+                    <img alt=""  className="btn" src={xIcon} onClick={() => props.hidePopup()}  />
+                </div>
+                <div className="popup-main">
+                    <div className="cart popup-cart"></div>
+                    <div className="popup-info">
+                        <div className="popup-info-part">
+                            <img  alt=""src={windIconDark} />
+                            <div className="popup-info-part-text" >
+                                <h3>Wind</h3>
+                                <h4><b>Velocity: </b>2 km/h </h4>
+                                <h4><b>Direction: </b>333°</h4>
+                            </div>
                         </div>
-                    </div>
-                    <div className="popup-info-part">
-                        <img  alt="" src={rainIconDark}/>
-                        <div className="popup-info-part-text">
-                            <h3>Rain</h3>
-                            <h4><b>Percentage: </b>0 % </h4>
+                        <div className="popup-info-part">
+                            <img  alt="" src={rainIconDark}/>
+                            <div className="popup-info-part-text">
+                                <h3>Rain</h3>
+                                <h4><b>Percentage: </b>0 % </h4>
+                            </div>
                         </div>
-                    </div>
-                    <div className="popup-info-part">
-                        <img  alt="" src={cloudsIconDark} />
-                        <div className="popup-info-part-text last-part">
-                            <h3>Clouds</h3>
-                            <h4><b>Percentage: </b>46 % </h4>
+                        <div className="popup-info-part">
+                            <img  alt="" src={cloudsIconDark} />
+                            <div className="popup-info-part-text last-part">
+                                <h3>Clouds</h3>
+                                <h4><b>Percentage: </b>46 % </h4>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+            {props.isShowPopup && <Shadow />}
+        </>
     )
 }
 const mapStateToProps = state => {
     return{
         currentWeatherOfDay:state.currentWeatherOfDay,
-        isToggle:state.isToggle
+        isToggle:state.isToggle,
+        isShowPopup:state.isShowPopup
     }
 }
-export default connect(mapStateToProps)(Popup)
+export default connect(mapStateToProps,{hidePopup})(Popup)
