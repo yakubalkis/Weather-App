d711d910270703a61523181b7ee7178bd3bb8123
yakubalkis <yakupalkiis@gmail.com>
2022-09-13 15:36:37 +0300

    Changed name

--- src/components/CustomFunctions/getDays.js
+++
@@ -1,7 +0,0 @@
-export default function getDays(){
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] 
-    const date = new Date()
-    const day = date.getDate() >= 10 ? date.getDate() : '0'+ date.getDate()
-    const dayName = days[date.getDay()]
-    return [day,dayName]
-}
