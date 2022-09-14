bd656eea14dd6ad9f317bcf876dc1fcd5b29fdcd
yakubalkis <yakupalkiis@gmail.com>
2022-09-14 13:06:22 +0300

    Fixed type of day variable

--- src/components/CustomFunctions/getDayData.js
+++ src/components/CustomFunctions/getDayData.js
@@ -2,7 +2,7 @@ export default function getDayData(){
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] 
     const date = new Date()
     const currentHour = date.getHours()
-    const day = date.getDate() >= 10 ? date.getDate() : '0'+ date.getDate()
+    const day = date.getDate() >= 10 ? date.getDate() : Number('0'+ date.getDate())
     const dayName = days[date.getDay()]
     return [day,dayName,currentHour]
 }
