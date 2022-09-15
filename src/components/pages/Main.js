0ddafc6b45791ed00378d65352cd81ca29b22dee
yakubalkis <yakupalkiis@gmail.com>
2022-09-15 04:05:09 +0300

    Removed unnecessary states

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,25 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
-import { showSidebarDeleteCity,getDataOfWillBeRemovedCity } from "../../redux/actions";
+import { showSidebarDeleteCity } from "../../redux/actions";
 import Popup from "./Popup";
 import CityWeatherWeekly from "./CityWeatherWeekly";
 import Shadow from "../Main-homepage/Shadow";
-
-
-
 import SideBarDeleteCity from "../WeatherCarts/SideBarDeleteCity";
 
 function Main(props){
 
     const theme = props.isToggle ? 'dark':'light'
-    console.log(props.allWeatherForecasts)
+    
    
     return (
             <>
                 <main className={"main"} >
                     <div className={"main-elements"}>
                         <h1>{props.viewedCityName}</h1>
-                        <h2>Weather for the next <b>6 days</b></h2>
+                        <h2>Weather for the <b>next days</b></h2>
                         <button onClick={() => {props.showSidebarDeleteCity()}} 
                                 className={`${theme}-modeForBtnAdd remove-btn btn`} >Remove City
                         </button>
@@ -36,10 +33,8 @@ const mapStateToProps = state => {
     return {
         isToggle:state.isToggle,
         viewedCityName:state.viewedCityName,
-        allWeatherForecasts:state.allWeatherForecasts,
-        infoOfSelectedCities:state.infoOfSelectedCities,
         isShowPopup:state.isShowPopup,
         isTakenWeeklyWeatherForecast:state.isTakenWeeklyWeatherForecast
     }
 }
-export default connect(mapStateToProps, {showSidebarDeleteCity,getDataOfWillBeRemovedCity})(Main)
+export default connect(mapStateToProps, {showSidebarDeleteCity})(Main)
