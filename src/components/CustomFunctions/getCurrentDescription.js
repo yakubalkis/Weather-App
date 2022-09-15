c4e4b515e695def41237c4d3a9205f455acccbda
yakubalkis <yakupalkiis@gmail.com>
2022-09-15 03:44:05 +0300

    Refactored function

--- src/components/CustomFunctions/getCurrentDescription.js
+++ src/components/CustomFunctions/getCurrentDescription.js
@@ -6,29 +6,28 @@ export default function getCurrentDescription(weeklyWeatherForecast){
     const currentHour = getDayData()[2]
     const daysWeather = weeklyWeatherForecast.filter((item) => Number(item.dt_txt[8]+item.dt_txt[9]) !== currentDay)
   
-    const descriptionsAndTemps = []
+    const weatherData = []
     console.log(daysWeather)
-    for(let i = 1; i < daysWeather.length; i++){
+    for(let i = 1; i < daysWeather.length; i++){  
         const hour1 = Number(daysWeather[i].dt_txt[11]+daysWeather[i].dt_txt[12])
         const hour2 = Number(daysWeather[i-1].dt_txt[11]+daysWeather[i-1].dt_txt[12])
         
-        if(currentHour >= 21  && hour1===21 && i !== daysWeather.length-1){
-            const description = hour1 - currentHour > currentHour - hour2 ? daysWeather[i-1].weather[0].description : daysWeather[i].weather[0].description
-            const currentTemp = hour1 - currentHour > currentHour - hour2 ? daysWeather[i-1].main.temp : daysWeather[i].main.temp
-            descriptionsAndTemps.push({description,currentTemp})
-        }
-        else if(hour1 >= currentHour && currentHour > hour2 ){
-            const description = hour1 - currentHour > currentHour - hour2 ? daysWeather[i-1].weather[0].description : daysWeather[i].weather[0].description
-            const currentTemp = hour1 - currentHour > currentHour - hour2 ? daysWeather[i-1].main.temp : daysWeather[i].main.temp
-            descriptionsAndTemps.push({description,currentTemp})
+        if(currentHour===0 && hour1===0){
+            weatherData.push(daysWeather[i])
         }
         else if(i === daysWeather.length -1){
-            const description = daysWeather[i].weather[0].description
-            const currentTemp = daysWeather[i].main.temp
-            descriptionsAndTemps.push({description,currentTemp})
+            weatherData.push(daysWeather[i])
+        }
+        else if(currentHour >= 21  && hour1===21 && i !== daysWeather.length-1){
+            const data = hour1 - currentHour > currentHour - hour2 ? daysWeather[i-1] : daysWeather[i]
+            weatherData.push(data)
+        }
+        else if(hour1 >= currentHour && currentHour > hour2 ){
+            const data = hour1 - currentHour > currentHour - hour2 ? daysWeather[i-1] : daysWeather[i]
+            weatherData.push(data)
         }
     } 
-    console.log(descriptionsAndTemps)
-   return descriptionsAndTemps
+    console.log(weatherData)
+   return weatherData
     
 }  
