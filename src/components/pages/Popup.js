2162606c1ab1c9a8c5e5517e209cfe6890aeb466
yakubalkis <yakupalkiis@gmail.com>
2022-09-15 04:06:24 +0300

    Edited props in Cart  component

--- src/components/pages/Popup.js
+++ src/components/pages/Popup.js
@@ -14,8 +14,7 @@ function Popup(props){
     const xIcon = props.isToggle ? xIconDark : xIconLight
     const theme = props.isToggle ? 'dark' : 'light'
     let rainPercentage=0
-    const monthNames = ["January", "February", "March", "April", "May", "June",
-                        "July", "August", "September", "October", "November", "December"];
+   
     
     
     for(let key in props.currentWeatherOfDay?.rain){ // because of rain object has '3h' key 
@@ -26,11 +25,11 @@ function Popup(props){
 
     const cart = Object.keys(props.currentWeatherOfDay).length !== 0 ? 
        <Cart 
-            weatherState={props.currentWeatherOfDay.weather[0].description}
-            currentTemp={props.currentWeatherOfDay.main.temp} 
-            feelsLike={props.currentWeatherOfDay.main.feels_like}
-            humidity={props.currentWeatherOfDay.main.humidity}
-            day={props.currentWeatherOfDay.dt_txt[8]+props.currentWeatherOfDay.dt_txt[9]}
+            weatherState={props.currentWeatherOfDay[0].weather[0].description}
+            currentTemp={props.currentWeatherOfDay[0].main.temp} 
+            feelsLike={props.currentWeatherOfDay[0].main.feels_like}
+            humidity={props.currentWeatherOfDay[0].main.humidity}
+            day={props.currentWeatherOfDay[1]}
             city={props.data[1]}
             className={'popup-cart'}
         /> 
@@ -42,7 +41,7 @@ function Popup(props){
         <>
             <div className={`popup ${theme}-modeSidebar`} style={{display: props.isShowPopup ? 'block':'none'}}>
                 <div className="popup-header">
-                    <p>Weather Info from <b>{monthNames[Number(props.currentWeatherOfDay?.dt_txt[5]+props.currentWeatherOfDay?.dt_txt[6])-1]} {props.currentWeatherOfDay?.dt_txt[8]+props.currentWeatherOfDay?.dt_txt[9]}th</b></p>
+                    <p>Weather Info from <b>{props.currentWeatherOfDay[2]} {props.currentWeatherOfDay[1]}th</b></p>
                     <img alt=""  className="btn" src={xIcon} onClick={() => props.hidePopup()}  />
                 </div>
                 <div className="popup-main">
@@ -52,8 +51,8 @@ function Popup(props){
                             <img  alt=""src={windIconDark} />
                             <div className="popup-info-part-text" >
                                 <h3>Wind</h3>
-                                <h4><b>Velocity: </b>{Math.round(props.currentWeatherOfDay?.wind?.speed)} km/h </h4>
-                                <h4><b>Direction: </b>{Math.round(props.currentWeatherOfDay?.wind?.deg)}°</h4>
+                                <h4><b>Velocity: </b>{Math.round(props.currentWeatherOfDay[0]?.wind?.speed)} km/h </h4>
+                                <h4><b>Direction: </b>{Math.round(props.currentWeatherOfDay[0]?.wind?.deg)}°</h4>
                             </div>
                         </div>
                         <div className="popup-info-part">
@@ -67,7 +66,7 @@ function Popup(props){
                             <img  alt="" src={cloudsIconDark} />
                             <div className="popup-info-part-text last-part">
                                 <h3>Clouds</h3>
-                                <h4><b>Percentage: </b>{props.currentWeatherOfDay?.clouds?.all} % </h4>
+                                <h4><b>Percentage: </b>{props.currentWeatherOfDay[0]?.clouds?.all} % </h4>
                             </div>
                         </div>
                     </div>
