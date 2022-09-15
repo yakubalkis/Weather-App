d046ecf8846abe6a2abbde651f73163f3a60ee64
yakubalkis <yakupalkiis@gmail.com>
2022-09-15 03:55:30 +0300

    Created custom func to get month name

---
+++ src/components/CustomFunctions/getMonthName.js
@@ -0,0 +1,7 @@
+export default function getMonthName(index){
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+    
+    return monthNames[index-1]
+
+}
