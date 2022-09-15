8bb9743c4116752df7b8fb6ee060890867e0cd91
yakubalkis <yakupalkiis@gmail.com>
2022-09-15 03:45:12 +0300

    Added monthName variable to return

--- src/components/CustomFunctions/getDayData.js
+++ src/components/CustomFunctions/getDayData.js
@@ -1,8 +1,11 @@
 export default function getDayData(){
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] 
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+                        "July", "August", "September", "October", "November", "December"]; 
     const date = new Date()
     const currentHour = date.getHours()
     const day = date.getDate() >= 10 ? date.getDate() : Number('0'+ date.getDate())
     const dayName = days[date.getDay()]
-    return [day,dayName,currentHour]
+    const monthName= monthNames[date.getMonth()]
+    return [day,dayName,currentHour,monthName]
 }
